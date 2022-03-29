e625640cdc46aaa7c793b18d6bd89e5b321c960c
Yuri <yurisilva474@gmail.com>
2022-03-29 13:50:34 -0300

    subindo-desafio

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,37 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    const id = Math.floor(Math.random() * 100);
+    const task = {
+      id,
+      title: newTaskTitle,
+      isComplete: false,
+    }
+    setTasks([...tasks, task])
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => {
+      if (task.id === id) {
+        return {
+          ...task,
+          isComplete: !task.isComplete
+        }
+      }
+      return task;
+    })
+
+    setTasks(newTasks)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const remainingTasks = tasks.filter(task => task.id !== id);
+    setTasks(remainingTasks)
   }
 
   return (
